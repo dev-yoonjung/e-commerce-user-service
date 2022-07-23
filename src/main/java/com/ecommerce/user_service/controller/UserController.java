137825fe76b716fb014418db15eb503f81999fea
dev-yoonjung <dear__yoon@naver.com>
2022-07-23 17:40:56 +0900

    fix: interlink user-service with api-gateway

--- src/main/java/com/ecommerce/user_service/controller/UserController.java
+++ src/main/java/com/ecommerce/user_service/controller/UserController.java
@@ -4,25 +4,28 @@ import com.ecommerce.user_service.dto.RequestUser;
 import com.ecommerce.user_service.dto.ResponseUser;
 import com.ecommerce.user_service.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
+@RequestMapping("/users")
 @RestController
 public class UserController {
 
     private final UserService userService;
 
+    private final Environment environment;
+
     @GetMapping("/health-check")
     public String status() {
-        return "It's working in user service";
+        return String.format(
+                "It's working in user service on port %s",
+                environment.getProperty("local.server.port"));
     }
 
-    @PostMapping("/users")
+    @PostMapping("")
     public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser requestUser) {
         ResponseUser responseUser = userService.createUser(requestUser);
 
